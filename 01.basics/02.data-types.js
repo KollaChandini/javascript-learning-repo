f1a61db330a1de5c95d8f9ec67515f5b99ba5a59
kolla chandini <kollachandini8@gmail.com>
2024-10-22 14:04:04 +0530

    committed files

---
+++ 01.basics/02.data-types.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello World") //we are using nodejs, not browser. so we'll get a ReferenceError: alert is not defined
+
+let name = "chandini";
+let isLoggedIn = false;
+let state;
+
+// Number
+// BigInt
+// String => we can use "" or '' (but "" is recommended)
+// boolean => true/false
+// null => standalone value
+// undefined => means value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ 01.basics/03.Conversion-or-Operation.js
@@ -0,0 +1,25 @@
+let score = "45";
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "45" => 45
+// 45grt => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "chandini";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "chandini" => true
+
+let anyNumber = 33;
+let stringNumber = String(anyNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
