142ada0386ede2401d6f51597426493f7edd50a6
kolla chandini <kollachandini8@gmail.com>
2024-10-26 11:03:44 +0530

    committed files

--- 02.basics/04_objects.js
+++ 02.basics/04_objects.js
@@ -15,19 +15,32 @@ const instaUser = {
   },
 };
 // console.log(instaUser.fullname.usefullname.firstname);
-const obj1={1:"a",2:"b",}
-const obj2={3:"a",4:"b",}
-const obj3={5:"a",6:"b",}
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj3 = { 5: "a", 6: "b" };
 
-const obj4=Object.assign({},obj1,obj2,obj3)
-console.log(obj4);
-const obj5={...obj1,...obj2,...obj3}
-console.log(obj5);
+const obj4 = Object.assign({}, obj1, obj2, obj3);
+// console.log(obj4);
+const obj5 = { ...obj1, ...obj2, ...obj3 };
+// console.log(obj5);
 
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+const course = {
+  courseName: "js in hindi",
+  price: "999",
+  courseInstructor: "Hitesh Choudhary",
+};
+// console.log(course["courseName"]);
+
+// console.log(course.courseInstructor);
+
+const { courseInstructor: instructor } = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
