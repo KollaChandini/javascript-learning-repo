78d4a4a5e1c9dd99a21ec9ab9af0177603f15c1d
kolla chandini <kollachandini8@gmail.com>
2024-10-27 19:17:32 +0530

    committed files

---
+++ 05.iterations.js/two.js
@@ -0,0 +1,31 @@
+//while
+
+// while (condition) {
+
+// }
+// let index = 0;
+// while (index <= 10) {
+// //   console.log(`value of index is ${index}`);
+//   index = index + 2;
+// }
+
+// let myArr = ["lilly", "rose", "jasmine"];
+// let index = 0;
+// while (index < myArr.length) {
+//   console.log(myArr[index]);
+//   index++;
+// }
+
+// do {
+
+// } while (condition);
+
+//kam pehle hotha aur condition baad me check hotha
+let score = 11;
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
+
+// The 'for' and 'while' loops are what we call 'entry control loops' because they check the condition before entering the loop. On the other hand, 'do-while' loops are known as 'exit control loops' since they ensure that the loop body is executed at least once before checking the condition.
+
